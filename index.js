12deb1b69f1fb32d9c8d65b38ecf823898252c04
carlosngv <carlosngv99@hotmail.com>
2021-05-03 01:32:03 -0600

    Rutas arregladas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 
 
-app.use('/queriess', router);
+app.use('/q', router);
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
